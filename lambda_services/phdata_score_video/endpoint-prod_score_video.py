aad35151564a4dc518234de46dbdd7973ea0f58b
admin <coop@phdata.io>
2020-08-07 14:56:38 +0000

    Updated Lambda service config

--- lambda_services/phdata_score_video/endpoint-prod_score_video.py
+++ lambda_services/phdata_score_video/endpoint-prod_score_video.py
@@ -1,5 +1,9 @@
 
 from  dataiku.apinode.predict.predictor import ClassificationPredictor
+
+import dataiku
+import base64
+
 import pandas as pd
 class MyPredictor(ClassificationPredictor):
     """The class for a classification Custom API node predictor"""
@@ -30,22 +34,54 @@ class MyPredictor(ClassificationPredictor):
 
         decision_series, proba_df and custom_keys_list must have the same number of rows than df.
         """
+        
+        dataiku.set_remote_dss('http://52.71.116.91:80', 'cooIYgi7VNXqTr5XIeEbLQRIis7jTEvm')
 
-        # Note: this sample uses the second form (decision_series, proba_df)
+        df = features_df
+        data_folder = dataiku.Folder('MzP4vBYB', project_key='PHDATAEMOTION')
+        
+        for ind, row in df.iterrows():
+            fname = 'ind_{}.mp4'.format(ind)
 
-        # Note: this sample "cheats" and always returns 5 predictions.
-        # You should actually return 1 prediction per row in the features_df
+            df.loc[ind, 'fname'] = fname
+            data_folder.upload_data(fname, base64.urlsafe_b64decode(row.b64_video.encode('utf-8')))
 
-        print "Features DataFrame %s" % features_df
+        client = dataiku.api_client()
+        project = client.get_project('PHDATAEMOTION')
+        scenario = project.get_scenario('PRODSCOREVIDEOS')
+        
+        scenario_run = scenario.run_and_wait()
+        success = scenario_run.get_info()['result']['outcome'] == 'SUCCESS'
 
-        # predictions, one per record (features_df row)
-        predictions = pd.Series(["good", "fair", "poor", "good", "poor"])
+        ds = project.get_dataset('ProdScoredVideos')
+        scores = pd.DataFrame(data=list(ds.iter_rows()), 
+                              columns=[c['name'] for c in ds.get_schema()['columns']])
+        scores = scores.set_index('video_path')
+    
+    
+        emotions = ['calm', 'sad', 'surprised', 'neutral', 
+            'fearful', 'angry', 'happy', 'disgust']
 
-        # optional probas for each class (may be None or a DataFrame with one column per class)
-        probas = pd.DataFrame({
-            'proba_good': pd.Series([.9, .6, .2, .7, .2]),
-            'proba_fair': pd.Series([.2, .7, .3, .3, .3]),
-            'proba_poor': pd.Series([.2, .6, .6, .3, .9])
-        })
+        df = df.drop(columns=['b64_video']).join(scores, on='fname')
 
-        return (predictions, probas)
+        for ind, row in df.iterrows():
+            max_val = 0
+            max_label = None
+            sum_val = 0
+
+            for e in emotions:
+                p = row['prediction_{}_avg'.format(e)]
+                sum_val += p
+                if p > max_val:
+                    max_val = p
+                    max_label = e
+
+            df.loc[ind, 'prediction'] = max_label
+
+            for e in emotions:
+                df.loc[ind, 'proba_{}'.format(e)] = row['prediction_{}_avg'.format(e)] / sum_val
+                
+        decisions = df.prediction
+        proba_df = df[['proba_{}'.format(e) for e in emotions]]
+            
+        return (decisions, proba_df)
